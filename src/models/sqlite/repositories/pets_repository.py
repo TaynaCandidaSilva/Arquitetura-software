59c41dd5293a16f68a9e5f70e44530a1f3c3edcb
TaynaCandidaSilva~ <tayna2507@gmail.com>
2024-11-25 13:33:44 -0300

    Testes: finalizando testes em Pets_repository

--- src/models/sqlite/repositories/pets_repository.py
+++ src/models/sqlite/repositories/pets_repository.py
@@ -1,15 +1,13 @@
 from typing import List
-
-from sqlalchemy.orm.exc import NoResultFound
-
 from src.models.sqlite.entities.pets import PetsTable
+from sqlalchemy.orm.exc import NoResultFound
 
 
 class PetsRepository:
     def __init__(self, db_connection) -> None:
         self.__db_connection = db_connection
 
-    def list_pets(self) -> List[PetsTable]:
+    def list_pets(self) -> List:
         with self.__db_connection as database:
             try:
                 pets = database.session.query(PetsTable).all()
@@ -20,7 +18,11 @@ class PetsRepository:
     def delete_pets(self, name: str) -> None:
         with self.__db_connection as database:
             try:
-                (database.session.query(PetsTable).filter_by(name=name).delete())
+                (
+                    database.session.query(PetsTable)
+                    .filter(PetsTable.name == name)
+                    .delete()
+                )
                 database.session.commit()
             except Exception as exception:
                 database.session.rollback()
--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -3,7 +3,7 @@ from .pets_repository import PetsRepository
 from src.models.sqlite.settings.connection import db_connection_handler
 
 
-db_connection_handler.connect_to_db()
+# db_connection_handler.connect_to_db()
 
 
 @pytest.mark.skip(reason="Interacao com o banco de dados")

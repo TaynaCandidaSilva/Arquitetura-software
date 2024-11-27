44b8c9864cf3e17bf3b4e50567766f84d8598de1
TaynaCandidaSilva~ <tayna2507@gmail.com>
2024-11-27 17:47:33 -0300

    Implementacao: classe People_repository.py e teste de insercao de People

--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -1,9 +1,9 @@
 import pytest
 from .pets_repository import PetsRepository
 from src.models.sqlite.settings.connection import db_connection_handler
+from .people_repository import PeopleRepository
 
-
-# db_connection_handler.connect_to_db()
+db_connection_handler.connect_to_db()
 
 
 @pytest.mark.skip(reason="Interacao com o banco de dados")
@@ -13,7 +13,19 @@ def test_list_pets():
     print(response)
 
 
+@pytest.mark.skip(reason="Interacao com o banco de dados")
 def test_delete_pet():
     name = "belinha"
     repo = PetsRepository(db_connection_handler)
     repo.delete_pets(name)
+
+
+@pytest.mark.skip(reason="Interacao com o banco de dados")
+def test_insert_person():
+    first_name = "teste name"
+    second_name = "test last name"
+    age = 77
+    pet_id = 2
+
+    repo = PeopleRepository(db_connection_handler)
+    repo.insert_people(first_name, second_name, age, pet_id)

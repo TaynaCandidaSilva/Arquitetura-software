480e117e223952fdff70e303fc2e0f7d0268ecc8
TaynaCandidaSilva~ <tayna2507@gmail.com>
2024-12-02 19:39:26 -0300

    Implementacao:continuacao da implementacao da classe People_repository.py e teste de GET People

--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -3,7 +3,7 @@ from .pets_repository import PetsRepository
 from src.models.sqlite.settings.connection import db_connection_handler
 from .people_repository import PeopleRepository
 
-db_connection_handler.connect_to_db()
+# db_connection_handler.connect_to_db()
 
 
 @pytest.mark.skip(reason="Interacao com o banco de dados")
@@ -29,3 +29,14 @@ def test_insert_person():
 
     repo = PeopleRepository(db_connection_handler)
     repo.insert_people(first_name, second_name, age, pet_id)
+
+
+@pytest.mark.skip(reason="Interacao com o banco de dados")
+def test_get_person():
+    person_id = 1
+
+    repo = PeopleRepository(db_connection_handler)
+    response = repo.get_person(person_id)
+    print()
+    print(response)
+    print(response.pet_name)

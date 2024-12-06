4b455ebc6f90784d4e895b99840325878703c5d7
TaynaCandidaSilva~ <tayna2507@gmail.com>
2024-12-06 15:57:14 -0300

    Alterando person creator controller

--- src/controllers/person_creator_controller.py
+++ src/controllers/person_creator_controller.py
@@ -1,6 +1,3 @@
-from src.models.sqlite.interfaces.people_repository import PeopleRepositoryInterface
-
-
 class PersonCreatorController:
-    def __init__(self, people_repository: PeopleRepositoryInterface) -> None:
-        self.__people_repository = people_repository
+    def __init__(self) -> None:
+        pass
